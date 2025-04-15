5e8abf5a7a0a08f9de28eb06b56ae062e35c9387
mohamed <iaazi.mohamed.1998@gmail.com>
2025-04-15 12:56:44 +0100

    i create the project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavBarComponent,AccueilComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AccueilComponent } from '../accueil/accueil.component';
+
+@Component({
+  selector: 'app-nav-bar',
+  imports: [AccueilComponent],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css'
+})
+export class NavBarComponent {
+
+}
