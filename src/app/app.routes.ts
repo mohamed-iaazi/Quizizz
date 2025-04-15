7df2276b2f949eccd006e27189c62b2d34c0d647
mohamed <iaazi.mohamed.1998@gmail.com>
2025-04-15 17:05:28 +0100

    i create the settings componant

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { HistoriqueComponent } from './historique/historique.component';
+import { SettingsComponent } from './settings/settings.component';
 
 export const routes: Routes = [
  {
@@ -16,5 +17,9 @@ export const routes: Routes = [
  {
     path: 'Historique',
     component: HistoriqueComponent
+ } , 
+ {
+   path :'Settings',
+   component : SettingsComponent
  }
 ];
