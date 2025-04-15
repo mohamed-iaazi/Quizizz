c6d4805c8ca0ad8ae8ef75409f4ef3b8ed54875c
mohamed <iaazi.mohamed.1998@gmail.com>
2025-04-15 17:01:29 +0100

    i create the routes for menu nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { HistoriqueComponent } from './historique/historique.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ {
+    path: 'accueil',
+    component : AccueilComponent 
+ },
+ {
+    path: 'Quiz',
+    component : QuizComponent
+ },
+ 
+ {
+    path: 'Historique',
+    component: HistoriqueComponent
+ }
+];
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { AccueilComponent } from '../accueil/accueil.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [AccueilComponent],
+  imports: [AccueilComponent,RouterLink],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+
+
 
 }
