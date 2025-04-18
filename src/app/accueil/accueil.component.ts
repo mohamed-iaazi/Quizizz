ac7eec298044f60ff21e55d12e23cea41df70444
mohamed <iaazi.mohamed.1998@gmail.com>
2025-04-18 10:51:51 +0100

    i add the observable to services

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { GetquestionService } from '../services/getquestion.service';
 
 @Component({
   selector: 'app-accueil',
@@ -9,6 +10,13 @@ import { Router } from '@angular/router';
   styleUrls: ['./accueil.component.css'], // ✅ Correct key name
 })
 export class AccueilComponent  {
+ Questions : any;
+
+  constructor(private QuestionService : GetquestionService){
+    this.Questions = this.QuestionService.getQuestion('25','easy')
+    this.Questions.subscribe((x : any) => console.log('qus')+ x)
+    
+  }
 
 
  
