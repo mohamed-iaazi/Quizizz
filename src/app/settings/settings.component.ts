93d10c30a7d7517d790078c2296dca917027c3bf
Mohamed <iaazi.mohamed.19998@gmail.com>
2025-04-21 19:08:31 +0100

    i add the settings section

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
 export class SettingsComponent {
+updateuserName() {
+  localStorage.setItem("userName",this.username);
+  console?.log(this.username)
+
+
+}
+  username : string =""+localStorage.getItem("userName")?.toString();
 
 }
